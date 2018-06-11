4081632fecc908dee1c365628583d6771e735c15
Amanda Malmin <amandamalmin@gmail.com>
2018-06-11 10:19:24 -0500

    Updating notes

---
+++ primitives.js
@@ -0,0 +1,14 @@
+//this is a one line comment
+/*
+This is a block comment
+It can go multio lines
+*/
+
+//Strings
+"JOE"
+"JOE "
+"3"
+"Hello\nWorld" // \n is new line character
+//Numbers
+
+//Variables
